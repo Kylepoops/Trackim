df45b2a4c618eb8b9a64174c2cdad8d0e2d44c26
Kylepoops <62238380+Kylepoops@users.noreply.github.com>
2021-07-14 08:36:40 +0800

    sequence

--- src/main/kotlin/cn/cubegarden/trackim/listener/InventoryListener.kt
+++ src/main/kotlin/cn/cubegarden/trackim/listener/InventoryListener.kt
@@ -4,13 +4,11 @@ import cn.cubegarden.trackim.compass.Updater
 import cn.cubegarden.trackim.compass.Updater.trackMap
 import cn.cubegarden.trackim.utils.Config
 import cn.cubegarden.trackim.utils.TrackimHolder
-import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 
 class InventoryListener: Listener {
